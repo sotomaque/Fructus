8fed3b416d9e134ed934d5e014554bee2be06621
Enrique <sotomaque@gmail.com>
2020-12-29 03:46:27 -0800

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Enrique Sotomayor on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
